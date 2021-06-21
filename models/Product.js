c0ff9f352f2541b34201fe556b2bfcd29dab9455
Mimi Vo <mimzy414@gmail.com>
2021-06-21 12:39:05 -0500

    fixed product.js

--- models/Product.js
+++ models/Product.js
@@ -12,10 +12,10 @@ Product.init(
     //====== define columns ======//
     // id 
     id: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-      primaryKey: true, 
-      autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     // product_name
     product_name: {
@@ -24,16 +24,19 @@ Product.init(
     },
     // stock
     stock: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
     },
     // price 
     price: {
       type: DataTypes.DECIMAL(10,2),
       allowNull: false,
-      defaultValue: 10,
       validate: {
-        isNumeric: true
+        isDecimal: true
       }
     },
     // category_id 
